b86001b08ff1d3c689697c1a00555c7ef1277272
GustavoHiguera97 <gustavohiguera9715@gmail.com>
2021-10-21 19:09:00 -0700

    most recent code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
-var searchBtn = document.getElementById("movie-searchBtn");
+var searchBtn = document.querySelector("#movie-searchBtn");
 
-searchBtn.addEventListener("click", function() {
-    var input = document.getElementById("user-input").value;
-    
-    console.log(input);
-});
+var displayMovieInfo = function () {
+    searchBtn.addEventListener("click", function () {
+        var userMovie = document.getElementById("user-input").value;
 
+        console.log(userMovie);
+
+        omdbApi(userMovie);
+    });
+};
+
+var omdbApi = function (userMovie) {
+    var omdbKey = "d9d0cc4d";
+    var omdbUrl = "http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + userMovie;
+
+    fetch(omdbUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+};
+
+displayMovieInfo();
