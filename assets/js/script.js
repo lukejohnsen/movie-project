03456ffa8908895dd43ff8a48ec31ebf99957404
GustavoHiguera97 <gustavohiguera9715@gmail.com>
2021-10-21 20:13:38 -0700

    fetching both apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,11 @@
 var searchBtn = document.querySelector("#movie-searchBtn");
 
 var displayMovieInfo = function () {
-    searchBtn.addEventListener("click", function () {
-        var userMovie = document.getElementById("user-input").value;
-
-        console.log(userMovie);
-
-        omdbApi(userMovie);
-    });
 };
 
 var omdbApi = function (userMovie) {
     var omdbKey = "d9d0cc4d";
-    var omdbUrl = "http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + userMovie;
+    var omdbUrl = "http://www.omdbapi.com/?t=" + userMovie + "&apikey=" + omdbKey;
 
     fetch(omdbUrl)
         .then(function (response) {
@@ -20,7 +13,39 @@ var omdbApi = function (userMovie) {
         })
         .then(function (data) {
             console.log(data);
+
+        var actorInfo = document.getElementById("actors");
+        actorInfo.append(data.Actors);
+
+        // will take data from omdb fetch
+        tastediveApi(data);
         });
 };
+
+var tastediveApi = function (movieData) {
+    var cors_preface = 'https://uofa21cors.herokuapp.com/';
+    var tastediveKey = "425881-GustavoH-440997PS"
+    var tastediveUrl = cors_preface + "https://tastedive.com/api/similar?q=" + movieData.Title + "&type=movie" + "&k=" +tastediveKey;
+
+    fetch(tastediveUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("tastedive", data);
+
+        });
+
+
+};
+
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    var userMovie = document.getElementById("user-input").value;
+
+    console.log(userMovie);
+
+    omdbApi(userMovie);
+});
 
 displayMovieInfo();
